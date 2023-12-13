717bd4fe53713b902206d8d02b3706c72f603440
RahulKanchi1 <150813823+RahulKanchi1@users.noreply.github.com>
2023-12-13 17:25:09 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,37 @@
+# printing elements
+print( ' this is printing statement ') 
+# variables
+n1 = 2
+n2 = 3
+# opertators
+print( n1 +n2)
+print( n1-n2) 
+print(n1 * n2 ) 
+print( n1 / n2)  
+n1 += 1
+print( n1)
+n2 *= 2
+print( n2)  
+print( n1 == n2)
+print( n1 == 20 and n2 == 6)  
+print( n1 == 20 or n2 == 6)
+# loop 
+for i in range( 15):
+    print( ' loop number ' , i)  
+    # conditonal statements
+if ( n2 == 5) :
+    print( ' The number is notequal to  6')
+else :
+    print(' the number is equal to 6' ) 
+    # function
+def printer( n):
+    for i in range( n):
+        print( ' loop number ' , i)  
+n = 8
+printer(n)
+# while loop in the program 
+while( n > 0):
+    print( n)
+    n -= 1  
+
+       
